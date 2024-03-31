eecaa781b1bced3a3def2a0d00d735e347efee39
성동식 <tjdehdtlr123@naver.com>
2024-04-01 00:43:34 +0900

    스프링부트 3.2이상 querydsl 추가함

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,40 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
 @SpringBootTest
+@Transactional
+@Commit
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		/*Hello result = queryFactory.select(qHello) //from 사용 안해서 에러남 추후에 더 가서 확인해야됨
+						.fetchFirst();
+
+		Assertions.assertThat(result).isEqualTo(hello);
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());*/
+
 	}
+
+
 
 }
