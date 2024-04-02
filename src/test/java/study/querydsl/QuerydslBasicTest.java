3ea266e60ec76898fa08f2268693daeca302a4fa
성동식 <tjdehdtlr123@naver.com>
2024-04-03 01:25:33 +0900

    querydsl 기능 추가

---
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,69 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.Team;
+
+import static org.assertj.core.api.Assertions.*;
+import static study.querydsl.entity.QMember.member;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+    JPAQueryFactory queryFactory;
+    @BeforeEach
+    public void before(){
+        queryFactory = new JPAQueryFactory(em);
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamB);
+
+        Member member3 = new Member("member3", 30, teamA);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL(){
+        //member1 찾기
+        Member findMember = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQuerydsl(){
+
+        //QMember m = new QMember("m"); //어떤 q로 설정한건지
+        //같은 테이블을 조인할때 q타입을 선언해서 사용하는것
+        Member findMember = queryFactory.select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+}
