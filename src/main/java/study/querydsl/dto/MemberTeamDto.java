a47862cd1ccfc7dc6c91043c7a1834d9a9050237
성동식 <tjdehdtlr123@naver.com>
2024-04-12 03:09:26 +0900

    querydsl 순수 JPA와 Querydsl

---
+++ src/main/java/study/querydsl/dto/MemberTeamDto.java
@@ -0,0 +1,28 @@
+package study.querydsl.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Data;
+
+@Data
+public class MemberTeamDto {
+
+    private Long id;
+
+    private String username;
+
+    private int age;
+
+    private Long teamId;
+
+    private String teamName;
+
+
+    @QueryProjection
+    public MemberTeamDto(Long id, String username, int age, Long teamId, String teamName) {
+        this.id = id;
+        this.username = username;
+        this.age = age;
+        this.teamId = teamId;
+        this.teamName = teamName;
+    }
+}
